f0c0f5315045896665bae4ebba4debbab2ece234
ksc27 <kellysc27@gmail.com>
2025-07-02 21:10:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+  const botao = document.getElementById("meuBotao");
+  const mensagemDiv = document.getElementById("mensagem");
+
+  botao.addEventListener("click", function() {
+   mensagemDiv.textContent = "Quando faltar sorte, faça sobrar atitude: o azar morre de medo de pessoas determinadas ^.^";
+  });
+
+  
